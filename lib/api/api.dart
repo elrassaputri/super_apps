466da1832eb4bbdf11b0cecf596640c8240c11f7
azarangga <azarangga@gmail.com>
2019-07-18 10:29:20 +0700

    18juli

---
+++ lib/api/api.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Api{
+  static var host = "http://10.204.100.120/super/";
+  static var app_login = host+"app_login.php";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:super_apps/ui/login_page.dart';
+import 'package:super_apps/style/theme.dart' as Theme;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Super Apps',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -18,9 +19,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new LoginPage(),
     );
   }
 }
@@ -96,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              //style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
