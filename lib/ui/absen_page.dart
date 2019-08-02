aa539682b9299287795582c85954ac19f77bfe0b
azar dwi angga purnama <azarangga@gmail.com>
2019-08-02 13:56:34 +0700

    Center vertical layout

--- lib/ui/absen_page.dart
+++ lib/ui/absen_page.dart
@@ -9,7 +9,7 @@ DateTime now = DateTime.now();
 String formattedDate = DateFormat('kk:mm').format(now);
 
 var absen = 'false';
-bool onLocation = false;
+bool onLocation = true;
 
 class FingerPrintAbsen extends StatefulWidget {
   FingerPrintAbsen({Key key}) : super(key: key);
@@ -98,6 +98,7 @@ class _FingerPrintAbsen extends State<FingerPrintAbsen> {
   @override
   Widget build(BuildContext context) {
     double widthDevice = MediaQuery.of(context).size.width;
+    double heightDevice = MediaQuery.of(context).size.height;
 
     return Scaffold(
       body: Container(
@@ -106,106 +107,123 @@ class _FingerPrintAbsen extends State<FingerPrintAbsen> {
           slivers: <Widget>[
             SliverList(
               delegate: SliverChildListDelegate([
-                Container(
-                  padding: EdgeInsets.only(top: 50, left: 16, right: 16),
-                  margin: EdgeInsets.only(bottom: 32),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: prefix0.MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      _statusOnLocation(),
-                      Container(
-                        child: Text(_timeString,
-                            style: TextStyle(
-                                fontSize: 24,
-                                color: Colors.white,
-                                fontWeight: prefix0.FontWeight.bold)),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      Builder(
-                        builder: (context) => GestureDetector(
-                          onTap: () {
-                            _changeStatusAbsen();
-                            if (onLocation == true) {
-                              if (absen == 'masuk') {
-                                Future.delayed(Duration(seconds: 1)).then((_) {
-                                  final snackBar = SnackBar(
-                                      content:
-                                          Text('Semangat pagi pagi pagi!!!'),
-                                      action: SnackBarAction(
-                                        label: 'OK',
-                                        onPressed: () {
-                                          // Some code to undo the change.
-                                        },
-                                      ));
-                                  Scaffold.of(context).showSnackBar(snackBar);
-                                });
-                              } else if (absen == 'pulang') {
-                                Future.delayed(Duration(milliseconds: 200))
-                                    .then((_) {
-                                  final snackBar = SnackBar(
-                                      content: Text('Pulang lu sana!!!'),
-                                      action: SnackBarAction(
-                                        label: 'OK',
-                                        onPressed: () {
-                                          // Some code to undo the change.
-                                        },
-                                      ));
-                                  Scaffold.of(context).showSnackBar(snackBar);
-                                });
-                              } else if (absen == 'false') {
-                                Future.delayed(Duration(seconds: 1)).then((_) {
-                                  final snackBar = SnackBar(
-                                      content: Text('loop back'),
-                                      action: SnackBarAction(
-                                        label: 'OK',
-                                        onPressed: () {
-                                          // Some code to undo the change.
-                                        },
-                                      ));
-                                  Scaffold.of(context).showSnackBar(snackBar);
-                                });
-                              }
-                            } else {
-                              Future.delayed(Duration(seconds: 1)).then((_) {
-                                final snackBar = SnackBar(
-                                    content: Text(
-                                        'Mohon datang ke kantor untuk melakukan absen!!'),
-                                    action: SnackBarAction(
-                                      label: 'OK',
-                                      onPressed: () {
-                                        // Some code to undo the change.
-                                      },
-                                    ));
-                                Scaffold.of(context).showSnackBar(snackBar);
-                              });
-                            }
-                          },
-                          child: Container(
-                            width: widthDevice,
-                            child: Image.asset(
-                                'assets/images/absen_masuk_fingerprint.gif'),
+                Column(
+                  crossAxisAlignment: prefix0.CrossAxisAlignment.center,
+                  mainAxisAlignment: prefix0.MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      color: Colors.yellow,
+                      height: heightDevice,
+                      padding: prefix0.EdgeInsets.only(top: heightDevice * .1, bottom: heightDevice * .1),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Container(
+                            padding: prefix0.EdgeInsets.only(left: widthDevice * .05, right: widthDevice * .05),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: prefix0.MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                _statusOnLocation(),
+                                Container(
+                                  child: Text(_timeString,
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          color: Colors.white,
+                                          fontWeight: prefix0.FontWeight.bold)),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
+                          Container(
+                            child: Row(
+                              children: <Widget>[
+                                Builder(
+                                  builder: (context) => GestureDetector(
+                                    onTap: () {
+                                      _changeStatusAbsen();
+                                      if (onLocation == true) {
+                                        if (absen == 'masuk') {
+                                          Future.delayed(Duration(milliseconds: 200)).then((_) {
+                                            final snackBar = SnackBar(
+                                                content:
+                                                Text('Semangat pagi pagi pagi!!!'),
+                                                action: SnackBarAction(
+                                                  label: 'OK',
+                                                  onPressed: () {
+                                                    // Some code to undo the change.
+                                                  },
+                                                ));
+                                            Scaffold.of(context).showSnackBar(snackBar);
+                                          });
+                                        } else if (absen == 'pulang') {
+                                          Future.delayed(Duration(milliseconds: 200))
+                                              .then((_) {
+                                            final snackBar = SnackBar(
+                                                content: Text('Pulang lu sana!!!'),
+                                                action: SnackBarAction(
+                                                  label: 'OK',
+                                                  onPressed: () {
+                                                    // Some code to undo the change.
+                                                  },
+                                                ));
+                                            Scaffold.of(context).showSnackBar(snackBar);
+                                          });
+                                        } else if (absen == 'false') {
+                                          Future.delayed(Duration(milliseconds: 200)).then((_) {
+                                            final snackBar = SnackBar(
+                                                content: Text('loop back'),
+                                                action: SnackBarAction(
+                                                  label: 'OK',
+                                                  onPressed: () {
+                                                    // Some code to undo the change.
+                                                  },
+                                                ));
+                                            Scaffold.of(context).showSnackBar(snackBar);
+                                          });
+                                        }
+                                      } else {
+                                        Future.delayed(Duration(milliseconds: 200)).then((_) {
+                                          final snackBar = SnackBar(
+                                              content: Text(
+                                                  'Mohon datang ke kantor untuk melakukan absen!!'),
+                                              action: SnackBarAction(
+                                                label: 'OK',
+                                                onPressed: () {
+                                                  // Some code to undo the change.
+                                                },
+                                              ));
+                                          Scaffold.of(context).showSnackBar(snackBar);
+                                        });
+                                      }
+                                    },
+                                    child: Container(
+                                      width: widthDevice,
+                                      child: Image.asset(
+                                          'assets/images/absen_masuk_fingerprint.gif'),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(top: 32),
+                                  child: _textStatusAbsen(),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(top: 32),
-                        child: _textStatusAbsen(),
-                      )
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ]),
             )
