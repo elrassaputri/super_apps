8caddcc8048478b1e247a9ff3f06de2adfefba4f
AstoMiwanda <astomiwanda08@.com>
2019-08-20 16:22:13 +0700

    merge main page logic

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Api{
   static var host = "http://10.204.100.120/super/";
   static var app_login = host+"app_login.php";
+  static var versi = "1";
 }
 
 class ListApi{
--- lib/ui/main_menu_page.dart
+++ lib/ui/main_menu_page.dart
@@ -66,6 +66,14 @@ class _MainMenuState extends State<MainMenu> {
     ['assets/icon/main_menu_page/video.svg', 'Video', '11'],
   ];
 
+  List imgList = [
+    'https://images.unsplash.com/photo-1502117859338-fd9daa518a9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1554321586-92083ba0a115?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1536679545597-c2e5e1946495?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1543922596-b3bbaba80649?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+    'https://images.unsplash.com/photo-1502943693086-33b5b1cfdf2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+  ];
+
   mainMenuHeaderLogo() {
     return Container(
       height: 54.0,
@@ -145,10 +153,23 @@ class _MainMenuState extends State<MainMenu> {
       height: widthDevice * .4,
       child: Carousel(
         boxFit: BoxFit.cover,
-        images: [
-          AssetImage('assets/images/quotes_3.jpg'),
-          AssetImage('assets/images/tis.jpg'),
-        ],
+        images: imgList.map((imgUrl) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: Image.network(
+                  imgUrl,
+                  fit: BoxFit.fill,
+                ),
+              );
+            },
+          );
+        }).toList(),
         autoplay: true,
         autoplayDuration: Duration(seconds: 4),
         dotBgColor: Theme.Colors.transparent,
